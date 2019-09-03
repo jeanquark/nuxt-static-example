f64b44c13a3c94328c7672bdff3c20a5a6c1d716
jeanquark <jm.kleger@gmail.com>
2019-09-03 16:43:40 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+     <div>
+       <h1><nuxt-link to="/">My Awesome Blog</nuxt-link></h1>
+       <div><nuxt-link to="/about">About</nuxt-link></div>
+       <div><nuxt-link to="/team">Team</nuxt-link></div>
+     </div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+
+</style>
